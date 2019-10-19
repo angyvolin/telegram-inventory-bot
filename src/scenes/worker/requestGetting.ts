178ff5a6ed90ccdc486a0597f0c5009ba84e7434
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-10-19 12:35:47 +0300

    Different requestGetting scenes were removed.

--- src/scenes/worker/requestGetting.ts
+++ src/scenes/worker/requestGetting.ts
@@ -17,28 +17,16 @@ requestGetting.command('start', async (ctx: any) => {
 
 // Точка входа в сцену
 requestGetting.enter(async (ctx: any) => {
-	const keyboard = Markup.inlineKeyboard([[Markup.callbackButton('Инструменты', 'instrument'), Markup.callbackButton('Фурнитура', 'furniture')], [Markup.callbackButton('Расходники', 'consumable'), Markup.callbackButton('Назад', 'back')]]).extra();
+	const keyboard = Markup.inlineKeyboard([[Markup.callbackButton('⏪ Назад', 'back')]]).extra();
 	await ctx.replyWithMarkdown('Выберите тип объектов, которые вы хотите получить', keyboard);
 });
 
 requestGetting.on('callback_query', async (ctx: any) => {
 	await ctx.answerCbQuery();
 	switch (ctx.callbackQuery.data) {
-		case 'instrument': {
-			await ctx.scene.leave();
-			await ctx.scene.enter('requestGettingInstrument');
-			break;
-		}
-		case 'furniture': {
-			await ctx.scene.leave();
-			await ctx.scene.enter('requestGettingFurniture');
-			break;
-		}
-		case 'consumable': {
-			await ctx.scene.leave();
-			await ctx.scene.enter('requestGettingConsumable');
-			break;
-		}
+		/*
+		 *
+		 */
 		case 'back': {
 			return KeyboardMessage.send(ctx, PersonType.WORKER);
 		}
