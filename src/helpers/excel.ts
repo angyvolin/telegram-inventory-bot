9f9d0f007b7157e4875db4f8231632c4175711fa
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-10-23 16:36:11 +0300

    Excel helper was created.

---
+++ src/helpers/excel.ts
@@ -0,0 +1,4 @@
+import ItemType from '../enums/ItemType';
+import Instrument, { IInstrument } from '../models/instrument';
+import Furniture, { IFurniture } from '../models/furniture';
+import Consumable, { IConsumable } from '../models/consumable';
