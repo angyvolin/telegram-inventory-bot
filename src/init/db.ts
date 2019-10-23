4a589ea711f25cda8d08ec28d210697817b97eb7
Vilen Kuprienko <pride2808@gmail.com>
2019-10-23 13:58:08 +0300

    Deprecation warnings were removed

--- src/init/db.ts
+++ src/init/db.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import config from '../config';
 import Logger from './logger.js';
+import Cell from '../models/cell';
 
 export default class DB {
 	private static url: string;
@@ -9,13 +10,26 @@ export default class DB {
 		// Проверка окружения и смена url базы данных
 		this.url = config.dbUrl;
 
+		mongoose.set('useNewUrlParser', true);
+		mongoose.set('useFindAndModify', false);
+		mongoose.set('useCreateIndex', true);
+		mongoose.set('useUnifiedTopology', true);
+
 		// Подключение к базе данных
-		mongoose.connect(this.url, { useNewUrlParser: true, keepAlive: true, useCreateIndex: true }, (err: any) => {
+		mongoose.connect(this.url, async (err: any) => {
 			if (err) {
 				Logger.fatal(`XXX Возникла ошибка при подключении к MongoDB! Текст ошибки: \n${err.message}`);
 				process.exit(1); // Выход из приложения
 			} else {
 				Logger.trace('>>> База данных подключена');
+				const alphabet = 'ABCD';
+
+				// Создаём сетку ячеек
+				for (let row of alphabet) {
+					for (let col = 1; col <= 9; col++) {
+						await Cell.findOneAndUpdate({row, col}, {row, col}, {upsert: true});
+					}
+				}
 			}
 		});
 	}
