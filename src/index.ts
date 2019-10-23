de80928e2929ff9b5fd85efb5614b42bf164c66f
Vilen Kuprienko <pride2808@gmail.com>
2019-10-23 17:41:45 +0300

    Generating table function was added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import DB from './init/db';
 import Handlers from './init/handlers';
 import Middlewares from './init/middlewares';
 import Scenes from './init/scenes';
+import { generateTable } from './helpers/excel';
 
 const main = async () => {
 	await DB.connect(); // подключаемся к БД
@@ -12,6 +13,8 @@ const main = async () => {
 	Middlewares.init(bot); // инициализируем прослойки
 	Scenes.init(bot); // инициализируем сцены
 	Handlers.init(bot); // инициализируем обработчики
+
+	await generateTable();
 };
 
 main();
